6a2b86265112fec97a8899fb2819303e2823049e
Mary Mazi <mazi.mary.o@gmail.com>
2017-06-08 10:09:30 +0100

    Properly indent code

--- app/index.js
+++ app/index.js
@@ -2,15 +2,18 @@
 'use strict';
 
 module.exports = {
-	fizzBuzz: (arg) => {
-        if (arg % 3 === 0 && arg % 5 === 0) {
-        return "FizzBuzz";
-        } else if (arg % 3 === 0) {
-        return "Fizz";
-        } else if (arg % 5 === 0) {
-        return "Buzz";
-        } else {
-        return arg;
-        }
-	}
+  fizzBuzz: (arg) => {
+    if (arg % 3 === 0 && arg % 5 === 0) {
+      return "FizzBuzz";
+    } 
+    else if (arg % 3 === 0) {
+      return "Fizz";
+    } 
+    else if (arg % 5 === 0) {
+      return "Buzz";
+    } 
+    else {
+      return arg;
+    }
+  }
 }
